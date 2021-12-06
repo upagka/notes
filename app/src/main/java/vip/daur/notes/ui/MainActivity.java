ec630183c8820d7027b32e5ba6418427ec844c55
upagka <upagka@yandex.com>
2021-12-06 22:51:50 +0600

    final changes

--- app/src/main/java/vip/daur/notes/ui/MainActivity.java
+++ app/src/main/java/vip/daur/notes/ui/MainActivity.java
@@ -1,11 +1,18 @@
 package vip.daur.notes.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentResultListener;
 
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 import vip.daur.notes.R;
 import vip.daur.notes.domain.Note;
+import vip.daur.notes.ui.detail.NoteDetailsActivity;
+import vip.daur.notes.ui.detail.NoteDetailsFragment;
+import vip.daur.notes.ui.list.NotesListFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,10 +26,51 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if(savedInstanceState != null && savedInstanceState.containsKey(ARG_NOTE)) {
+        if (savedInstanceState != null && savedInstanceState.containsKey(ARG_NOTE)) {
             selectedNote = savedInstanceState.getParcelable(ARG_NOTE);
 
+            if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                showDetails();
+            }
         }
+
+        getSupportFragmentManager()
+                .setFragmentResultListener(NotesListFragment.RESULT_KEY, this, new FragmentResultListener() {
+                    @Override
+                    public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle result) {
+                        selectedNote = result.getParcelable(NotesListFragment.ARG_NOTE);
+
+                        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                            showDetails();
+                        } else {
+
+                            getSupportFragmentManager()
+                                    .beginTransaction()
+                                    .add(R.id.notes_container_vertical, new NoteDetailsFragment())
+                                    .commit();
+
+//                            Intent intent = new Intent(MainActivity.this, NoteDetailsActivity.class);
+//                            intent.putExtra(NoteDetailsActivity.EXTRA_NOTE, selectedNote);
+//                            startActivity(intent);
+                        }
+                    }
+                });
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        if (selectedNote != null) {
+            outState.putParcelable(ARG_NOTE, selectedNote);
+        }
+    }
+
+    private void showDetails() {
+        Bundle bundle = new Bundle();
+        bundle.putParcelable(NoteDetailsFragment.ARG_NOTE, selectedNote);
+        getSupportFragmentManager()
+                .setFragmentResult(NoteDetailsFragment.KEY_RESULT, bundle);
     }
 
 }
