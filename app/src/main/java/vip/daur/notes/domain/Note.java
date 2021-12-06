dbe7de159071c80fcadf47e13ea5f9511bf4f3ea
daur-vip <mail@daur.vip>
2021-12-06 17:51:48 +0600

    added Note.java

--- app/src/main/java/vip/daur/notes/domain/Note.java
+++ app/src/main/java/vip/daur/notes/domain/Note.java
@@ -1,2 +1,56 @@
-package vip.daur.notes.domain;public class Note {
+package vip.daur.notes.domain;
+
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.util.Set;
+
+public class Note implements Parcelable {
+
+    private int id;
+
+    private String title;
+
+    private String text;
+
+    private Set<Integer> tags;
+
+    private int priority;
+
+    private String date;
+
+    protected Note(Parcel in) {
+        id = in.readInt();
+        title = in.readString();
+        text = in.readString();
+        priority = in.readInt();
+        date = in.readString();
+    }
+
+    public static final Creator<Note> CREATOR = new Creator<Note>() {
+        @Override
+        public Note createFromParcel(Parcel in) {
+            return new Note(in);
+        }
+
+        @Override
+        public Note[] newArray(int size) {
+            return new Note[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(id);
+        dest.writeString(title);
+        dest.writeString(text);
+        dest.writeInt(priority);
+        dest.writeString(date);
+    }
 }
