bcd9e610bbf921c9411f606e943698f1de91dd82
daur-vip <mail@daur.vip>
2021-12-07 17:33:15 +0600

    more changes

--- app/src/main/java/vip/daur/notes/ui/MainActivity.java
+++ app/src/main/java/vip/daur/notes/ui/MainActivity.java
@@ -4,13 +4,11 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentResultListener;
 
-import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 
 import vip.daur.notes.R;
 import vip.daur.notes.domain.Note;
-import vip.daur.notes.ui.detail.NoteDetailsActivity;
 import vip.daur.notes.ui.detail.NoteDetailsFragment;
 import vip.daur.notes.ui.list.NotesListFragment;
 
@@ -26,25 +24,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.notes_container, new NotesListFragment())
+                .addToBackStack("main")
+                .commit();
+
         if (savedInstanceState != null && savedInstanceState.containsKey(ARG_NOTE)) {
             selectedNote = savedInstanceState.getParcelable(ARG_NOTE);
 
             if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
-
-                getSupportFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.notes_container, new NotesListFragment())
-                        .addToBackStack(null)
-                        .commit();
                 showDetails();
             }
-        } else {
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.notes_container, new NotesListFragment())
-                    .addToBackStack(null)
-                    .commit();
-
         }
 
         getSupportFragmentManager()
@@ -59,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
 
                             getSupportFragmentManager()
                                     .beginTransaction()
-                                    .replace(R.id.notes_container, new NoteDetailsFragment())
+                                    .replace(R.id.notes_container, new NoteDetailsFragment().newInstance(selectedNote))
                                     .commit();
 
 //                            Intent intent = new Intent(MainActivity.this, NoteDetailsActivity.class);
